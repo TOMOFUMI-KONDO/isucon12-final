80889170e49daddfee015674d109f33ef667cc38
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-07 19:58:24 +0900

    add index to user_one_time_tokens

--- webapp/sql/3_schema_exclude_user_presents.sql
+++ webapp/sql/3_schema_exclude_user_presents.sql
@@ -244,7 +244,8 @@ CREATE TABLE `user_one_time_tokens` (
   `expired_at` bigint NOT NULL,
   `deleted_at` bigint default NULL,
   PRIMARY KEY (`id`),
-  UNIQUE uniq_token (`user_id`, `token`, `deleted_at`)
+  UNIQUE uniq_token (`user_id`, `token`, `deleted_at`),
+  KEY `token_idx` (`token`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_bin;
 
 /* 管理者権限のセッション管理 */
