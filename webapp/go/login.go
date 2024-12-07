bc77cfffbe49e626a37ffedc202a276e802f2c31
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2024-12-07 19:22:45 +0900

    removed unused field assignment

---
+++ webapp/go/login.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"database/sql"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+// login ログイン
+// POST /login
+func (h *Handler) login(c echo.Context) error {
+	defer c.Request().Body.Close()
+	req := new(LoginRequest)
+	if err := parseRequestBody(c, req); err != nil {
+		return errorResponse(c, http.StatusBadRequest, err)
+	}
+
+	requestAt, err := getRequestTime(c)
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, ErrGetRequestTime)
+	}
+
+	user := new(User)
+	query := "SELECT * FROM users WHERE id=?"
+	if err := h.DB.Get(user, query, req.UserID); err != nil {
+		if err == sql.ErrNoRows {
+			return errorResponse(c, http.StatusNotFound, ErrUserNotFound)
+		}
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+
+	isBan, err := h.checkBan(user.ID)
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+	if isBan {
+		return errorResponse(c, http.StatusForbidden, ErrForbidden)
+	}
+
+	if err = h.checkViewerID(user.ID, req.ViewerID); err != nil {
+		if err == ErrUserDeviceNotFound {
+			return errorResponse(c, http.StatusNotFound, err)
+		}
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+
+	tx, err := h.DB.Beginx()
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+	defer tx.Rollback() //nolint:errcheck
+
+	query = "UPDATE user_sessions SET deleted_at=? WHERE user_id=? AND deleted_at IS NULL"
+	if _, err = tx.Exec(query, requestAt, req.UserID); err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+	sessID, err := generateUUID()
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+	sess := &Session{
+		UserID:    req.UserID,
+		SessionID: sessID,
+		CreatedAt: requestAt,
+		UpdatedAt: requestAt,
+		ExpiredAt: requestAt + 86400,
+	}
+	query = "INSERT INTO user_sessions(user_id, session_id, created_at, updated_at, expired_at) VALUES (?, ?, ?, ?, ?)"
+	if _, err := tx.Exec(query, sess.UserID, sess.SessionID, sess.CreatedAt, sess.UpdatedAt, sess.ExpiredAt); err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+
+	// 同日にすでにログインしているユーザはログイン処理をしない
+	if isCompleteTodayLogin(time.Unix(user.LastActivatedAt, 0), time.Unix(requestAt, 0)) {
+		user.UpdatedAt = requestAt
+		user.LastActivatedAt = requestAt
+
+		query = "UPDATE users SET updated_at=?, last_activated_at=? WHERE id=?"
+		if _, err := tx.Exec(query, requestAt, requestAt, req.UserID); err != nil {
+			return errorResponse(c, http.StatusInternalServerError, err)
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			return errorResponse(c, http.StatusInternalServerError, err)
+		}
+
+		return successResponse(c, &LoginResponse{
+			ViewerID:         req.ViewerID,
+			SessionID:        sess.SessionID,
+			UpdatedResources: makeUpdatedResources(requestAt, user, nil, nil, nil, nil, nil, nil),
+		})
+	}
+
+	user, loginBonuses, presents, err := h.loginProcess(tx, req.UserID, requestAt)
+	if err != nil {
+		if err == ErrUserNotFound || err == ErrItemNotFound || err == ErrLoginBonusRewardNotFound {
+			return errorResponse(c, http.StatusNotFound, err)
+		}
+		if err == ErrInvalidItemType {
+			return errorResponse(c, http.StatusBadRequest, err)
+		}
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return errorResponse(c, http.StatusInternalServerError, err)
+	}
+
+	return successResponse(c, &LoginResponse{
+		ViewerID:         req.ViewerID,
+		SessionID:        sess.SessionID,
+		UpdatedResources: makeUpdatedResources(requestAt, user, nil, nil, nil, nil, loginBonuses, presents),
+	})
+}
+
+type LoginRequest struct {
+	ViewerID string `json:"viewerId"`
+	UserID   int64  `json:"userId"`
+}
+
+type LoginResponse struct {
+	ViewerID         string           `json:"viewerId"`
+	SessionID        string           `json:"sessionId"`
+	UpdatedResources *UpdatedResource `json:"updatedResources"`
+}
--- webapp/go/main.go
+++ webapp/go/main.go
@@ -785,16 +785,9 @@ func (h *Handler) createUser(c echo.Context) error {
 		ExpiredAt: requestAt + 86400,
 	}
 	query = "INSERT INTO user_sessions(user_id, session_id, created_at, updated_at, expired_at) VALUES (?, ?, ?, ?, ?)"
-	result, err = tx.Exec(query, sess.UserID, sess.SessionID, sess.CreatedAt, sess.UpdatedAt, sess.ExpiredAt)
-	if err != nil {
+	if _, err = tx.Exec(query, sess.UserID, sess.SessionID, sess.CreatedAt, sess.UpdatedAt, sess.ExpiredAt); err != nil {
 		return errorResponse(c, http.StatusInternalServerError, err)
 	}
-
-	sID, err := result.LastInsertId()
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	sess.ID = sID
 
 	err = tx.Commit()
 	if err != nil {
@@ -820,133 +813,6 @@ type CreateUserResponse struct {
 	ViewerID         string           `json:"viewerId"`
 	SessionID        string           `json:"sessionId"`
 	CreatedAt        int64            `json:"createdAt"`
-	UpdatedResources *UpdatedResource `json:"updatedResources"`
-}
-
-// login ログイン
-// POST /login
-func (h *Handler) login(c echo.Context) error {
-	defer c.Request().Body.Close()
-	req := new(LoginRequest)
-	if err := parseRequestBody(c, req); err != nil {
-		return errorResponse(c, http.StatusBadRequest, err)
-	}
-
-	requestAt, err := getRequestTime(c)
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, ErrGetRequestTime)
-	}
-
-	user := new(User)
-	query := "SELECT * FROM users WHERE id=?"
-	if err := h.DB.Get(user, query, req.UserID); err != nil {
-		if err == sql.ErrNoRows {
-			return errorResponse(c, http.StatusNotFound, ErrUserNotFound)
-		}
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-
-	isBan, err := h.checkBan(user.ID)
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	if isBan {
-		return errorResponse(c, http.StatusForbidden, ErrForbidden)
-	}
-
-	if err = h.checkViewerID(user.ID, req.ViewerID); err != nil {
-		if err == ErrUserDeviceNotFound {
-			return errorResponse(c, http.StatusNotFound, err)
-		}
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-
-	tx, err := h.DB.Beginx()
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	defer tx.Rollback() //nolint:errcheck
-
-	query = "UPDATE user_sessions SET deleted_at=? WHERE user_id=? AND deleted_at IS NULL"
-	if _, err = tx.Exec(query, requestAt, req.UserID); err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	sessID, err := generateUUID()
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	sess := &Session{
-		UserID:    req.UserID,
-		SessionID: sessID,
-		CreatedAt: requestAt,
-		UpdatedAt: requestAt,
-		ExpiredAt: requestAt + 86400,
-	}
-	query = "INSERT INTO user_sessions(user_id, session_id, created_at, updated_at, expired_at) VALUES (?, ?, ?, ?, ?)"
-	result, err := tx.Exec(query, sess.UserID, sess.SessionID, sess.CreatedAt, sess.UpdatedAt, sess.ExpiredAt)
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-
-	sID, err := result.LastInsertId()
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-	sess.ID = sID
-
-	// 同日にすでにログインしているユーザはログイン処理をしない
-	if isCompleteTodayLogin(time.Unix(user.LastActivatedAt, 0), time.Unix(requestAt, 0)) {
-		user.UpdatedAt = requestAt
-		user.LastActivatedAt = requestAt
-
-		query = "UPDATE users SET updated_at=?, last_activated_at=? WHERE id=?"
-		if _, err := tx.Exec(query, requestAt, requestAt, req.UserID); err != nil {
-			return errorResponse(c, http.StatusInternalServerError, err)
-		}
-
-		err = tx.Commit()
-		if err != nil {
-			return errorResponse(c, http.StatusInternalServerError, err)
-		}
-
-		return successResponse(c, &LoginResponse{
-			ViewerID:         req.ViewerID,
-			SessionID:        sess.SessionID,
-			UpdatedResources: makeUpdatedResources(requestAt, user, nil, nil, nil, nil, nil, nil),
-		})
-	}
-
-	user, loginBonuses, presents, err := h.loginProcess(tx, req.UserID, requestAt)
-	if err != nil {
-		if err == ErrUserNotFound || err == ErrItemNotFound || err == ErrLoginBonusRewardNotFound {
-			return errorResponse(c, http.StatusNotFound, err)
-		}
-		if err == ErrInvalidItemType {
-			return errorResponse(c, http.StatusBadRequest, err)
-		}
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, err)
-	}
-
-	return successResponse(c, &LoginResponse{
-		ViewerID:         req.ViewerID,
-		SessionID:        sess.SessionID,
-		UpdatedResources: makeUpdatedResources(requestAt, user, nil, nil, nil, nil, loginBonuses, presents),
-	})
-}
-
-type LoginRequest struct {
-	ViewerID string `json:"viewerId"`
-	UserID   int64  `json:"userId"`
-}
-
-type LoginResponse struct {
-	ViewerID         string           `json:"viewerId"`
-	SessionID        string           `json:"sessionId"`
 	UpdatedResources *UpdatedResource `json:"updatedResources"`
 }
 
